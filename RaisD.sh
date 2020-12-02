0bb862ce12030ca7098c9e13fa26aab22f111ee1
Jimi92 <44607500+Jimi92@users.noreply.github.com>
2020-12-02 16:14:27 +0100

    Update RaisD.sh

--- RaisD.sh
+++ RaisD.sh
@@ -15,19 +15,6 @@ cd /home/taliadoros/Desktop/phd_projects/Cb/selection_fungicide_resistance/resis
 
 
 
-#/home/taliadoros/software/raisd-master/RAiSD -I CM008499.1_analysis.recode.vcf -n Cb_Chr1.out -f -B 6188355 76324 -R -w 70
-#/home/taliadoros/software/raisd-master/RAiSD -I CM008500.1_analysis.recode.vcf -n Cb_Chr2.out -f -B 4222505 77617 -R -w 70
-#/home/taliadoros/software/raisd-master/RAiSD -I CM008501.1_analysis.recode.vcf -n Cb_Chr3.out -f -B 4196595 74465 -R -w 70
-#/home/taliadoros/software/raisd-master/RAiSD -I CM008502.1_analysis.recode.vcf -n Cb_Chr4.out -f -B 4173231 66779 -R -w 70
-#/home/taliadoros/software/raisd-master/RAiSD -I CM008503.1_analysis.recode.vcf -n Cb_Chr5.out -f -B 4165208 62174 -R -w 70
-#/home/taliadoros/software/raisd-master/RAiSD -I CM008504.1_analysis.recode.vcf -n Cb_Chr6.out -f -B 2985383 60399 -R -w 70
-#/home/taliadoros/software/raisd-master/RAiSD -I CM008505.1_analysis.recode.vcf -n Cb_Chr7.out -f -B 2967425 69823 -R -w 70
-#/home/taliadoros/software/raisd-master/RAiSD -I CM008506.1_analysis.recode.vcf -n Cb_Chr8.out -f -B 2636389 50344 -R -w 70
-#/home/taliadoros/software/raisd-master/RAiSD -I CM008507.1_analysis.recode.vcf -n Cb_Chr9.out -f -B 2174017 37683 -R -w 70
-#/home/taliadoros/software/raisd-master/RAiSD -I CM008508.1_analysis.recode.vcf -n Cb_Chr10.out -f -B 1557915 28698 -R-w 70
-
-
-
 
 
 
